c5ccd67d781a2485371a8e1864f93e84797bec89
Jochem Broekhoff <broekhoff.jochem@gmail.com>
2015-01-15 19:01:19 +0100

    Removed the [storage] section of Sonycer.ini Updated the /index.html file to a nice error page. Added some documentation to the Config class (/system/Config.class.php)

--- system/Config.class.php
+++ system/Config.class.php
@@ -16,6 +16,12 @@ class Config{
         $this->file = dirname(__FILE__).'/../Sonaycer.ini';
         $this->parsed_ini_file = parse_ini_file($this->file, true);
     }
+    /**
+     * Get a configuration item from Sonaycer.ini
+     * @param string $section Section of Sonaycer.ini
+     * @param sting $key Key of the Section
+     * @return string
+     */
     function getItem($section='', $key=''){
         if(!empty($section) and !empty($key)){
             return $this->parsed_ini_file[$section][$key];
